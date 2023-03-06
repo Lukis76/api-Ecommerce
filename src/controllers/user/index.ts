1447473f481d5531b3ab0b3242d3a9157400f434
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-06 12:48:33 -0300

    barrel functions user

---
+++ src/controllers/user/index.ts
@@ -0,0 +1,21 @@
+import { userId } from "./userId";
+import { allUsers } from "./allUsers";
+import { login } from "./login";
+import { register } from "./register";
+import { block } from "./block";
+import { handleRefreshToken } from "./handleRefreshToken";
+import { logOut } from "./logOut";
+import { unBlock } from "./unBlock";
+import { updatePassword } from "./updatePassword";
+//////////////////////////////////////////////////////
+export {
+  allUsers,
+  login,
+  register,
+  userId,
+  unBlock,
+  logOut,
+  handleRefreshToken,
+  block,
+  updatePassword,
+};
