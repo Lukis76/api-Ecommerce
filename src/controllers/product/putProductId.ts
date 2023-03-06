7a40eb88b7a85192968273cf878376123936597f
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-06 12:45:47 -0300

    updated product id

---
+++ src/controllers/product/putProductId.ts
@@ -0,0 +1,27 @@
+import Product from "../../models/product";
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+///////////////////////////////////////////////////////////////////////////
+export const putProductId = asyncHandler(async (req: Request, res: Response) => {
+  const {id} = req.params
+  try {
+    //--------------------------------------
+    const { title, description } = req.body;
+    //----------------------------------------------------------------
+    const normalize = (str: string) => str.trim().replace(/\s+/g, " ")
+    //----------------------------------------------------------------
+    const slugTitle = title.trim().replace(/\s+/g, "-").toLowerCase();
+    //----------------------------------------------------------------
+    req.body.slug = slugTitle;
+    req.body.title = normalize(title);
+    req.body.description = normalize(description)
+    //-------------------------------------------------
+    const product = req.body;
+    //-----------------------------------------------
+    const updateProduct = await Product.findByIdAndUpdate({id}, product, {new: true});
+    //-----------------------------------------------
+    res.json(updateProduct);
+  } catch (err) {
+    throw new Error(err?.toString() || "Error inesperado");
+  }
+});
