3502bc893bdf65cd0ab8979ec1c3fc4e7680ce49
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-06 12:48:53 -0300

    user login

---
+++ src/controllers/user/login.ts
@@ -0,0 +1,51 @@
+import { generateToken } from "../../config/jwtToken";
+import asyncHandler from "express-async-handler";
+import { Response, Request } from "express";
+import User from "../../models/user";
+import { refreshToken } from "../../config/refreshToken";
+///////////////////////////////////////////////////////////////////////////////
+export const login = asyncHandler(async (req: Request, res: Response) => {
+  //==========================================================================
+  const { email, password } = req.body;
+  //===================================
+  // Check user exist
+  const findUser = await User.findOne({ email });
+  //=============================================
+  //@ts-ignore
+  const checkPass = await findUser.isPasswordMatched(password);
+  //===========================================================
+  if (findUser && checkPass) {
+    //-----------------------------------------------
+    const refToken = await refreshToken(findUser.id);
+    //-----------------------------------------------
+    await User.findByIdAndUpdate(
+      findUser.id,
+      {
+        refreshToken: refToken,
+      },
+      {
+        new: true,
+      }
+    );
+    //-------------------------------------
+    res.cookie("refreshToken", refToken, {
+      httpOnly: true, // access not navigator
+      maxAge: 72 * 60 * 60 * 1000, // time expired cookie
+      // secure: true,  // Production
+      // sameSite: "lax" // dist domains
+    });
+    //-----------------------------------
+    res.json({
+      id: findUser.id,
+      firstname: findUser.firstname,
+      lastname: findUser.lastname,
+      email: findUser.email,
+      mobile: findUser.mobile,
+      role: findUser.role,
+      token: generateToken(findUser._id.toString()),
+    });
+    //-----------------------------------------------
+  } else {
+    throw new Error("Invalid Credentials");
+  }
+});
