34375610f69b2fcc052fb5fd8a924760246580c6
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-06 12:45:36 -0300

    barrel functions product/s

---
+++ src/controllers/product/index.ts
@@ -0,0 +1,7 @@
+import { create } from "./create";
+import { allProducts } from "./allProducts";
+import { getProductId } from "./getProductId";
+import { putProductId } from "./putProductId";
+import { deleteProductId } from "./deleteProductId";
+
+export { create, allProducts, getProductId, putProductId, deleteProductId };
