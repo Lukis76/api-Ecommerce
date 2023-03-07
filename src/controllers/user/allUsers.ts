2698c9ffd6b1e1456735d40f1aec660768b36bca
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 20:41:52 -0300

    refactor error handling

--- src/controllers/user/allUsers.ts
+++ src/controllers/user/allUsers.ts
@@ -1,12 +1,13 @@
 import asyncHandler from "express-async-handler";
 import { Response, Request } from "express";
 import User from "../../models/user";
+import { CError } from "../../utils/err";
 ///////////////////////////////////////////////////////////////////////////////
 export const allUsers = asyncHandler(async (_req: Request, res: Response) => {
   try {
     const users = await User.find();
     res.json(users);
   } catch (err) {
-    throw new Error(err?.toString() || "Error inesperado");
+    throw CError(err, "Get all users");
   }
 });
