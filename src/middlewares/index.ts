ed7c3f923e2575a9c0b494f009bb94a993962ae3
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 20:46:10 -0300

    create barrel middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,4 @@
+import { auth, isAdmin, RequestType } from "./auth";
+import { notFound, errorHandler } from "./error";
+///////////////////////////////////////////////////////////////
+export { auth, isAdmin, RequestType, notFound, errorHandler };
