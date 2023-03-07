e0af0436d7c8f90cd4bda25e6a730bcc4667cb40
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 20:45:39 -0300

    refactor error handling

--- src/controllers/user/updatePassword.ts
+++ src/controllers/user/updatePassword.ts
@@ -1,43 +1,28 @@
-
-import { Request, Response } from "express";
+import { Response } from "express";
 import asyncHandler from "express-async-handler";
 import User from "../../models/user";
-import { RequestType } from "../../middlewares/auth"
+import { RequestType } from "../../middlewares/auth";
 import { validateMongo } from "../../utils/validateMongo";
-///////////////////////////////////////////////////////////////////////////
+import { CError } from "../../utils/err";
+////////////////////////////////////////////////////////////////////////////////////////
 export const updatePassword = asyncHandler(async (req: RequestType, res: Response) => {
-  //----------------------------------------------------------------------
-  const {id} = req.user
-  const {password} = req.body
-  
-  validateMongo(id)
-
-  const user = await User.findById(id)
-
-  if(password && user) {
-    user.password = password
-    const updatePassword = await user?.save()
-    res.json(updatePassword)
-  } else {
-    res.json(user)
-  }
-
-
-
-
-
-
-
-
-
-
+  //------------------------------------------------------------------------------------
+  const { id } = req.user;
+  const { password } = req.body;
+  //----------------------------
   try {
-    const block = await User.findByIdAndUpdate(id, { isBlocked: true }, { new: true });
-    //---------------------------------------------------------------------------------
-    res.json({
-      msg: "User Blocked",
-    });
+    validateMongo(id);
+    //-----------------------------------
+    const user = await User.findById(id);
+    //-----------------------------------
+    if (password && user) {
+      user.password = password;
+      const updatePassword = await user?.save();
+      res.json(updatePassword);
+    } else {
+      res.json(user);
+    }
   } catch (err) {
-    throw new Error(err as string);
+    throw CError(err, "Updated password user");
   }
 });
