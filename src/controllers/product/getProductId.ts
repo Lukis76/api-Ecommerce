c3fe34dbb19bd3c0899c7c7337fcc2ad5f566818
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 20:41:20 -0300

    refactorerror handling

--- src/controllers/product/getProductId.ts
+++ src/controllers/product/getProductId.ts
@@ -2,6 +2,7 @@
 import Product from "../../models/product"
 import asyncHandler from "express-async-handler"
 import {Request, Response } from "express"
+import { CError } from "../../utils/err"
 
 export const getProductId = asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params
@@ -9,7 +10,7 @@ export const getProductId = asyncHandler(async (req: Request, res: Response) => 
     const findProduct = await Product.findById(id) 
     res.json(findProduct)
   } catch (err) {
-    throw new Error(err?.toString() || "Error inesperado");
+    throw CError(err, "Get product");
   }
 
 })
