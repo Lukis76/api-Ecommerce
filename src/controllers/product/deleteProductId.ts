42feb228f8e73424bc6ad4a76aaf8e742a14a811
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 20:41:12 -0300

    refactor error handling

--- src/controllers/product/deleteProductId.ts
+++ src/controllers/product/deleteProductId.ts
@@ -2,15 +2,18 @@
 import Product from "../../models/product";
 import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
-///////////////////////////////////////////////////////////////////////////
+import { CError } from "../../utils/err";
+/////////////////////////////////////////////////////////////////////////////////////
 export const deleteProductId = asyncHandler(async (req: Request, res: Response) => {
+  //--------------------------------------------------------------------------------
   const {id} = req.params
+  //---------------------
   try {
-    //-----------------------------------------------
+    //---------------------------------------------------------
     const deleteProduct = await Product.findOneAndDelete({id});
-    //-----------------------------------------------
+    //---------------------------------------------------------
     res.json(deleteProduct);
   } catch (err) {
-    throw new Error(err?.toString() || "Error inesperado");
+    throw CError(err, "deleted product");
   }
 });
