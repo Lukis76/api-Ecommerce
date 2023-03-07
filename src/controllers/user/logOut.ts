988860112533c7c0292492002db95896e45593a6
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 20:44:01 -0300

    refactor error handling

--- src/controllers/user/logOut.ts
+++ src/controllers/user/logOut.ts
@@ -1,19 +1,24 @@
 import asyncHandler from "express-async-handler";
 import { Response, Request } from "express";
 import User from "../../models/user";
+import { CError } from "../../utils/err";
 ////////////////////////////////////////////////////////////////////////////
 export const logOut = asyncHandler(async (req: Request, res: Response) => {
   //------------------------------------------------------------------------
   const { refreshToken } = req.cookies;
-  //----------------------------------------------------------------
-  if (!refreshToken) throw new Error("No refresh token in cookies");
-  //----------------------------------------------------------------
-  await User.findOneAndUpdate(refreshToken, { refreshToken: "" });
-  //---------------------------------------------------------------
-  res.clearCookie("refreshToken", {
-    httpOnly: true,
-    secure: true,
-  });
-  //------------------
-  res.sendStatus(204);
+  try {
+    //----------------------------------------------------------------
+    if (!refreshToken) throw new Error("No refresh token in cookies");
+    //----------------------------------------------------------------
+    await User.findOneAndUpdate(refreshToken, { refreshToken: "" });
+    //---------------------------------------------------------------
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+    });
+    //------------------
+    res.sendStatus(204);
+  } catch (err) {
+    throw CError(err, "LogOut user");
+  }
 });
