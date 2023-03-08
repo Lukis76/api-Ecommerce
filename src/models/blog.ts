6d455d8c9e89265422b93b3a1c361ddd936d2b39
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-07 22:26:27 -0300

    add blog model

---
+++ src/models/blog.ts
@@ -0,0 +1,71 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "./user";
+
+export interface IBlog {
+  title: string;
+  description: string;
+  category: string;
+  views: number;
+  isLiked: boolean;
+  isDisliked: boolean;
+  likes: Array<IUser>;
+  dislikes: Array<IUser>;
+  images: string;
+  createdAt: Date;
+}
+
+const blogschema = new Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    images: {
+      type: String,
+      default: "http://example-image.com",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+export default model<IBlog>("Blog", blogschema);
