0583c0b3e478d12c2700fe130e137ac4c24731e3
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-09 12:02:38 -0300

    controller upated brand

---
+++ src/controllers/brand/putBrandId.ts
@@ -0,0 +1,20 @@
+import Brand from "../../models/brand";
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import { validateMongo } from "../../utils/validateMongo";
+import { CustomError } from "../../utils/err";
+
+export const putBrandId = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    validateMongo(id);
+    const updateBrand = await Brand.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updateBrand) throw new Error("Updated brand and find by id failed");
+    res.json({
+      status: "success",
+      updateBrand,
+    });
+  } catch (err) {
+    CustomError(res, err, "Updated brand");
+  }
+});
