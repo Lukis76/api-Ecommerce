1d1ccb5804450eb5228acd35dee85412033abd28
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-09 12:06:47 -0300

    add wish list in product barrel

--- src/controllers/product/index.ts
+++ src/controllers/product/index.ts
@@ -3,5 +3,6 @@ import { allProducts } from "./allProducts";
 import { getProductId } from "./getProductId";
 import { putProductId } from "./putProductId";
 import { deleteProductId } from "./deleteProductId";
+import { wishList } from "./wishList";
 
-export { create, allProducts, getProductId, putProductId, deleteProductId };
+export { create, allProducts, getProductId, putProductId, deleteProductId, wishList };
