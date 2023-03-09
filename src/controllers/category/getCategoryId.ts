8688de43155af887497b400bf355945a6d95a942
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:07:48 -0300

    get gategory > title

---
+++ src/controllers/category/getCategoryId.ts
@@ -0,0 +1,21 @@
+
+import Category from "../../models/category";
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import { validateMongo } from "../../utils/validateMongo";
+import { CError, CustomError } from "../../utils/err";
+
+export const getCategoryId = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    validateMongo(id)
+    const getCategory = await Category.findById(id);
+    if(!getCategory) throw new Error("Get category and find by id failed")
+    res.json({
+      status: "success",
+      getCategory,
+    });
+  } catch (err) {
+    CustomError(res, err, "Get category");
+  }
+});
