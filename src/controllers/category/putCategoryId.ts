5b8cdae32804e156329c998823caf15e3edc7adf
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-09 12:04:44 -0300

    ref import custom error and deleted console.log

--- src/controllers/category/putCategoryId.ts
+++ src/controllers/category/putCategoryId.ts
@@ -2,14 +2,13 @@ import Category from "../../models/category";
 import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
 import { validateMongo } from "../../utils/validateMongo";
-import { CError, CustomError } from "../../utils/err";
+import { CustomError } from "../../utils/err";
 
 export const putCategoryId = asyncHandler(async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     validateMongo(id);
     const updateCategory = await Category.findByIdAndUpdate(id, req.body, { new: true });
-    console.log("update category faind => ", updateCategory);
     if (!updateCategory) throw new Error("Updated category and find by id failed");
     res.json({
       status: "success",
