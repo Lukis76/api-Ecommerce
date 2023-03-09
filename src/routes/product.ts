1334c6e77b4a4a6a27c20a55a771c79fef66c7e8
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-09 12:08:35 -0300

    add route wish list

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { auth as middlewareAuth, isAdmin } from "../middlewares/auth";
-import { allProducts, getProductId, create, putProductId, deleteProductId } from "../controllers/product";
+import {
+  allProducts,
+  getProductId,
+  create,
+  putProductId,
+  deleteProductId,
+  wishList,
+} from "../controllers/product";
 
 const router = Router();
 
 router.post("/create", middlewareAuth, isAdmin, create);
+router.put("/wishlist", middlewareAuth, wishList);
 router.get("/:id", getProductId);
 router.put("/:id", middlewareAuth, isAdmin, putProductId);
 router.delete("/:id", middlewareAuth, isAdmin, deleteProductId);
