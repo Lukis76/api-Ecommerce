605638c8ded0d2fc8639fe392b3d9c14b30167b8
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:54:16 -0300

    add allCategory

--- src/controllers/category/index.ts
+++ src/controllers/category/index.ts
@@ -2,5 +2,6 @@ import { create } from "./create";
 import { putCategoryId } from "./putCategoryId";
 import { deleteCategoryId } from "./deleteCategoryId";
 import { getCategoryId } from "./getCategoryId";
+import { allCategory } from "./allCategory";
 
-export { create, putCategoryId, deleteCategoryId, getCategoryId };
+export { create, putCategoryId, deleteCategoryId, getCategoryId, allCategory };
