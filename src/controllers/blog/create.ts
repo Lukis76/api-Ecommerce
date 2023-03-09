8e3b14d23e70cc6294fdcf6babe8dae7b04624cb
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:04:17 -0300

    create blog

--- src/controllers/blog/create.ts
+++ src/controllers/blog/create.ts
@@ -5,23 +5,14 @@ import { Request, Response } from "express";
 import { validateMongo } from "../../utils/validateMongo";
 import { CError } from "../../utils/err";
 
-
-
-export const create = asyncHandler( async (req: Request, res: Response) => {
-
- try {
-  
-  const newBlog = await Blog.create(req.body)
-  res.json({
+export const create = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.json({
       status: "success",
       newBlog,
-    })
-
-
-
-
-
- } catch (err) {
- throw CError(err, "created Blog") 
- } 
-}) 
+    });
+  } catch (err) {
+    throw CError(err, "Created Blog");
+  }
+});
