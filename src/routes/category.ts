6694e6f52a7067cd34100267d587991212252e65
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:56:18 -0300

    add route allcategori and delete middlewareAuth, isAdmin in get category id

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { auth as middlewareAuth, isAdmin } from "../middlewares/auth";
-import { create, putCategoryId, deleteCategoryId, getCategoryId } from "../controllers/category";
+import { create, putCategoryId, deleteCategoryId, getCategoryId, allCategory } from "../controllers/category";
 
 const router = Router();
 
 router.post("/create", middlewareAuth, isAdmin, create);
 router.put("/:id", middlewareAuth, isAdmin, putCategoryId);
 router.delete("/:id", middlewareAuth, isAdmin, deleteCategoryId);
-router.get("/:id", middlewareAuth, isAdmin, getCategoryId);
+router.get("/:id", getCategoryId);
+router.get("/", allCategory);
 
 export default router;
