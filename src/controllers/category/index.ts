285b5a399753f829b56eca02fb2e54da792a03d4
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:08:02 -0300

    barrel category

---
+++ src/controllers/category/index.ts
@@ -0,0 +1,6 @@
+import { create } from "./create";
+import { putCategoryId } from "./putCategoryId";
+import { deleteCategoryId } from "./deleteCategoryId";
+import { getCategoryId } from "./getCategoryId";
+
+export { create, putCategoryId, deleteCategoryId, getCategoryId };
