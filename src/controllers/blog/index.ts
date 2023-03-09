6e06e8dbf58964738ed17694fbb631b78b56202b
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:06:12 -0300

    barrel blog

--- src/controllers/blog/index.ts
+++ src/controllers/blog/index.ts
@@ -1,3 +1,9 @@
 import { create } from "./create";
+import { putBlogId } from "./putBlogId";
+import { getBlogId } from "./getBlogId";
+import { allBlogs } from "./allBlogs";
+import { deleteBlogId } from "./deleteBlogId";
+import { like } from "./like";
+import { disLike } from "./disLike";
 
-export { create };
+export { create, putBlogId, getBlogId, allBlogs, deleteBlogId, like, disLike };
