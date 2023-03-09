c1f6c5a3d0c3ce809cd75c5d84e9767f05e7d8f6
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:11:24 -0300

    model category

---
+++ src/models/category.ts
@@ -0,0 +1,17 @@
+import { Schema, Types, model } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+      unique: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Category", categorySchema);
