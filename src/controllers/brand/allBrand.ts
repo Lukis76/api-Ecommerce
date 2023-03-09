8abce792d801b766339e7eb8f5156a4f840c5ef5
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-09 12:01:03 -0300

    controller all brand

---
+++ src/controllers/brand/allBrand.ts
@@ -0,0 +1,17 @@
+import Brand from "../../models/brand";
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import { CustomError } from "../../utils/err";
+
+export const allBrand = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const allBrand = await Brand.find();
+    if (!allBrand) throw new Error("Get find all brand failed");
+    res.json({
+      status: "success",
+      allBrand,
+    });
+  } catch (err) {
+    CustomError(res, err, "Get all brand");
+  }
+});
