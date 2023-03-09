56f43b4dc405794e6ba0dccee4f6be78fb12142f
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-08 22:14:18 -0300

    middle refactor custom error catch

--- src/utils/err.ts
+++ src/utils/err.ts
@@ -1,15 +1,28 @@
+import { Response } from "express";
+
 export const CError = (err: any, title: string) => {
-  console.error(err);
+  // console.error(err);
   const { name, message } = err;
   if (name && message) {
+    console.log(" one => ", name, message);
     return {
       name,
-      message,
-    };
+      message
+    }
   } else {
+    console.log(" two => ", name, message);
     return {
       name: `${title}`,
       message: "Error indefinido",
     };
   }
+};
+
+export const CustomError = (res: Response, err: any, customName: string) => {
+  res.status(404).json({
+    status: "Failed",
+    code: 404,
+    name: `${err?.name} || ${customName}`,
+    message: err?.message,
+  });
 };
