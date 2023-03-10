d0685451a0d06bb346115653abbb49704218e614
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-10 18:34:07 -0300

    add controller update in route for id

--- src/routes/coupon.ts
+++ src/routes/coupon.ts
@@ -1,14 +1,11 @@
-
 import { Router } from "express";
 import { auth as middlewareAuth, isAdmin } from "../middlewares/auth";
-import {
-  create,
-  allCoupons
-} from "../controllers/coupon";
+import { create, allCoupons, putCoupon } from "../controllers/coupon";
 
 const router = Router();
 
 router.post("/create", middlewareAuth, isAdmin, create);
 router.get("/", allCoupons);
+router.put("/:id", middlewareAuth, isAdmin, putCoupon);
 
 export default router;
