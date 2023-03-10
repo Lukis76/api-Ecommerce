213c53ca1f04cdc570c79b3181886e8dd644e4ac
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-10 18:25:13 -0300

    add route get all coupons

--- src/routes/coupon.ts
+++ src/routes/coupon.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 import { auth as middlewareAuth, isAdmin } from "../middlewares/auth";
 import {
   create,
+  allCoupons
 } from "../controllers/coupon";
 
 const router = Router();
 
 router.post("/create", middlewareAuth, isAdmin, create);
+router.get("/", allCoupons);
 
 export default router;
