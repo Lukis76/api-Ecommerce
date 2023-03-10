a329584e866811e7c912491138b4964e871abd94
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-10 18:38:32 -0300

    add deleted coupon in barrel

--- src/controllers/coupon/index.ts
+++ src/controllers/coupon/index.ts
@@ -1,5 +1,6 @@
 import { create } from "./create";
 import { allCoupons } from "./allCoupons";
 import { putCoupon } from "./putCoupon";
+import { deleteCoupon } from "./deleteCoupon";
 
-export { create, allCoupons, putCoupon };
+export { create, allCoupons, putCoupon, deleteCoupon };
