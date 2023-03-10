053fa26975b77c854f0b7832ed46cd7ab6865fbf
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-10 18:39:05 -0300

    add controler deleted coupon in routes for id

--- src/routes/coupon.ts
+++ src/routes/coupon.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { auth as middlewareAuth, isAdmin } from "../middlewares/auth";
-import { create, allCoupons, putCoupon } from "../controllers/coupon";
+import { create, allCoupons, putCoupon, deleteCoupon } from "../controllers/coupon";
 
 const router = Router();
 
 router.post("/create", middlewareAuth, isAdmin, create);
 router.get("/", allCoupons);
 router.put("/:id", middlewareAuth, isAdmin, putCoupon);
+router.delete("/:id", middlewareAuth, isAdmin, deleteCoupon);
 
 export default router;
