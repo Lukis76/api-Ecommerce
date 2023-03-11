5f4d12ab55b85034fabf49b4e1f3348825b49ac9
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-11 16:43:50 -0300

    muddleware upload imagen

--- src/middlewares/uploadImgs.ts
+++ src/middlewares/uploadImgs.ts
@@ -1,17 +1,18 @@
-import multer, { FileFilterCallback, Multer } from "multer";
-import { Request, Response, NextFunction } from "express";
+import multer, { FileFilterCallback } from "multer";
+import { Response, NextFunction } from "express";
 import sharp from "sharp";
 import path from "path";
-
+///////////////////////////////////////////
 const multerStorage = multer.diskStorage({
   destination: (_req, _file, cb) => {
-    cb(null, path.join(__dirname, "../public/images"));
+    cb(null, path.join(__dirname, "../../public/images"));
   },
   filename: (_req, file: Express.Multer.File, cb) => {
     const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    cb(null, file.filename + "-" + uniqueName + ".jpeg");
+    cb(null, file.fieldname + "-" + uniqueName + ".png");
   },
 });
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const multerFilter = (_req: Express.Request, file: Express.Multer.File, cb: FileFilterCallback) => {
   if (file.mimetype.startsWith("image")) {
@@ -20,14 +21,16 @@ const multerFilter = (_req: Express.Request, file: Express.Multer.File, cb: File
     cb(null, false);
   }
 };
+/////////////////////////////////////
 
 export const uploadPhoto = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
-  limits: { fieldSize: 2_000_000 },
-});
+  limits: { fieldSize: 5_000_000 },
+}).array("images", 10);
+//////////////////////////////////////////////////////////////////////////////////////////
 
-export const productImgResize = async (req: Express.Request, _res: Response, next: NextFunction) => {
+export const productImgResize = async (req: any, _res: Response, next: NextFunction) => {
   if (!req.files || !req.file) return next();
 
   if (Array.isArray(req.files)) {
@@ -35,22 +38,21 @@ export const productImgResize = async (req: Express.Request, _res: Response, nex
       req.files.map(async (file: Express.Multer.File) => {
         await sharp(file.path)
           .resize(300, 300)
-          .toFormat("jpeg")
-          .jpeg({ quality: 70 })
+          .toFormat("png")
+          .png({ quality: 70 })
           .toFile(`public/images/products/${file.filename}`);
       })
     );
   } else {
     await sharp(req.file?.path)
       .resize(300, 300)
-      .toFormat("jpeg")
-      .jpeg({ quality: 70 })
+      .toFormat("png")
+      .png({ quality: 70 })
       .toFile(`public/images/products/${req.file?.filename}`);
   }
+  next();
 };
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 export const blogImgResize = async (req: Express.Request, _res: Response, next: NextFunction) => {
   if (!req.files || !req.file) return next();
@@ -60,27 +62,17 @@ export const blogImgResize = async (req: Express.Request, _res: Response, next: 
       req.files.map(async (file: Express.Multer.File) => {
         await sharp(file.path)
           .resize(300, 300)
-          .toFormat("jpeg")
-          .jpeg({ quality: 70 })
+          .toFormat("png")
+          .png({ quality: 70 })
           .toFile(`public/images/blogs/${file.filename}`);
       })
     );
   } else {
     await sharp(req.file?.path)
       .resize(300, 300)
-      .toFormat("jpeg")
-      .jpeg({ quality: 70 })
+      .toFormat("png")
+      .png({ quality: 70 })
       .toFile(`public/images/blogs/${req.file?.filename}`);
   }
+  next();
 };
-
-
-
-
-
-
-
-
-
-
-
