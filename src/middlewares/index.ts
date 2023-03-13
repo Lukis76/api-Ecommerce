c8e86c5c3b2ab0c453ad39c2e547fd49398fa75f
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-13 12:54:12 -0300

    add imports/exports in barrel middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,14 @@
 import { auth, isAdmin, RequestType } from "./auth";
 import { notFound, errorHandler } from "./error";
+import { uploadPhoto, blogImgResize, productImgResize } from "./uploadImgs";
 ///////////////////////////////////////////////////////////////
-export { auth, isAdmin, RequestType, notFound, errorHandler };
+export {
+  auth,
+  isAdmin,
+  RequestType,
+  notFound,
+  errorHandler,
+  productImgResize,
+  blogImgResize,
+  uploadPhoto,
+};
