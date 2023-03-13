0f0227fa8b6cc3f60a6f6593d0ab7ca81e83d4a8
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-13 12:47:00 -0300

    add in barrel upload image

--- src/controllers/blog/index.ts
+++ src/controllers/blog/index.ts
@@ -5,5 +5,6 @@ import { allBlogs } from "./allBlogs";
 import { deleteBlogId } from "./deleteBlogId";
 import { like } from "./like";
 import { disLike } from "./disLike";
+import { uploadImgs } from "./uploadImgs";
 
-export { create, putBlogId, getBlogId, allBlogs, deleteBlogId, like, disLike };
+export { create, putBlogId, getBlogId, allBlogs, deleteBlogId, like, disLike, uploadImgs };
