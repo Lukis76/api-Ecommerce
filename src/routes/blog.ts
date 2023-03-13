bc48765c3861d53316b3d6dd971bb7aad75677f6
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-13 12:59:17 -0300

    add routes

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { auth as middlewareAuth, isAdmin } from "../middlewares/auth";
-import { create, putBlogId, getBlogId, allBlogs, deleteBlogId, like, disLike } from "../controllers/blog";
+import { auth as middlewareAuth, isAdmin, blogImgResize, uploadPhoto } from "../middlewares";
+import {
+  create,
+  putBlogId,
+  getBlogId,
+  allBlogs,
+  deleteBlogId,
+  like,
+  disLike,
+  uploadImgs,
+} from "../controllers/blog";
 
 const router = Router();
 
 router.post("/create", middlewareAuth, isAdmin, create);
+router.put("/upload/:id", middlewareAuth, isAdmin, uploadPhoto, blogImgResize, uploadImgs);
 router.put("/like", middlewareAuth, like);
 router.put("/dislike", middlewareAuth, disLike);
 router.put("/:id", middlewareAuth, isAdmin, putBlogId);
